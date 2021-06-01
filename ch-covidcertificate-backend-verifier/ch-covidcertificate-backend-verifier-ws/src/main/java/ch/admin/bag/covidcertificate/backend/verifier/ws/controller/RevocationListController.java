aeb0c4bb971baff75806db280934570697397c23
lmeinen <lmeinen@student.ethz.ch>
2021-06-01 13:04:21 +0200

    Added skeleton for revocation list controller

---
+++ ch-covidcertificate-backend-verifier/ch-covidcertificate-backend-verifier-ws/src/main/java/ch/admin/bag/covidcertificate/backend/verifier/ws/controller/RevocationListController.java
@@ -0,0 +1,90 @@
+/*
+ * Copyright (c) 2021 Ubique Innovation AG <https://www.ubique.ch>
+ *
+ * This Source Code Form is subject to the terms of the Mozilla Public
+ * License, v. 2.0. If a copy of the MPL was not distributed with this
+ * file, You can obtain one at https://mozilla.org/MPL/2.0/.
+ *
+ * SPDX-License-Identifier: MPL-2.0
+ */
+
+package ch.admin.bag.covidcertificate.backend.verifier.ws.controller;
+
+import ch.admin.bag.covidcertificate.backend.verifier.ws.utils.RestTemplateHelper;
+import ch.ubique.openapi.docannotations.Documentation;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.RequestEntity;
+import org.springframework.http.ResponseEntity;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.client.HttpStatusCodeException;
+import org.springframework.web.client.RestTemplate;
+import org.springframework.web.util.UriComponentsBuilder;
+
+@Controller
+@RequestMapping("/v1")
+public class RevocationListController {
+
+    private static final Logger logger = LoggerFactory.getLogger(RevocationListController.class);
+
+    private final String revocationListEndpoint;
+    private final RestTemplate rt;
+
+    // TODO: Instantiate bean in BaseConfig & define property for endpoint url
+    public RevocationListController(String revocationListEndpoint) {
+        this.revocationListEndpoint = revocationListEndpoint;
+        // TODO: Implement
+        this.rt = RestTemplateHelper.getRestTemplate();
+    }
+
+    @Documentation(
+            description = "get list of revoked certificates",
+            responses = {"200 => full list of revoked certificates"})
+    @CrossOrigin(origins = {"https://editor.swagger.io"})
+    @GetMapping(value = "/revocation-list")
+    public @ResponseBody ResponseEntity<RevocationResponse> getCerts() {
+        final var response = new RevocationResponse();
+        final List<String> certs = new ArrayList<>();
+        final var uri = UriComponentsBuilder.fromHttpUrl(revocationListEndpoint).build().toUri();
+        final RequestEntity<Void> requestEntity =
+                RequestEntity.get(uri).headers(createDownloadHeaders()).build();
+        try {
+            final var responseEntity = rt.exchange(requestEntity, String[].class);
+            final var body = responseEntity.getBody();
+            if (body != null) {
+                certs.addAll(Arrays.asList(body));
+            }
+        } catch (HttpStatusCodeException e) {
+            logger.info("Request returned error code: {} {}", e.getStatusCode(), e.getStatusCode().getReasonPhrase());
+            throw e;
+        }
+        response.setRevokedCerts(certs);
+        return ResponseEntity.ok().body(response);
+    }
+
+    private HttpHeaders createDownloadHeaders() {
+        HttpHeaders headers = new HttpHeaders();
+        headers.add(HttpHeaders.ACCEPT, "application/json");
+        return headers;
+    }
+
+    public static class RevocationResponse {
+        private List<String> revokedCerts;
+
+        public List<String> getRevokedCerts() {
+            return revokedCerts;
+        }
+
+        public void setRevokedCerts(List<String> revokedCerts) {
+            this.revokedCerts = revokedCerts;
+        }
+    }
+}
