58a1f4b39fb38c430a51a755e346bf3a12da5459
Felix Haller <haller@ubique.ch>
2021-06-08 15:15:50 +0200

    add validduration for list requests

--- ch-covidcertificate-backend-verifier/ch-covidcertificate-backend-verifier-model/src/main/java/ch/admin/bag/covidcertificate/backend/verifier/model/RevocationResponse.java
+++ ch-covidcertificate-backend-verifier/ch-covidcertificate-backend-verifier-model/src/main/java/ch/admin/bag/covidcertificate/backend/verifier/model/RevocationResponse.java
@@ -1,6 +1,7 @@
 package ch.admin.bag.covidcertificate.backend.verifier.model;
 
 import ch.ubique.openapi.docannotations.Documentation;
+import java.time.Duration;
 import java.util.List;
 
 public class RevocationResponse {
@@ -9,11 +10,24 @@ public class RevocationResponse {
             example = "[\"urn:uvci:01:CH:F0FDABC1708A81BB1A843891\"]")
     private List<String> revokedCerts;
 
+    @Documentation(
+            description = "describes how long the list response is valid for in ms",
+            example = "172800000")
+    private Duration validDuration = Duration.ofHours(48);
+
     public List<String> getRevokedCerts() {
         return revokedCerts;
     }
 
     public void setRevokedCerts(List<String> revokedCerts) {
         this.revokedCerts = revokedCerts;
+    }
+
+    public Long getValidDuration() {
+        return validDuration.toMillis();
+    }
+
+    public void setValidDuration(Long durationInMs) {
+        this.validDuration = Duration.ofMillis(durationInMs);
     }
 }
--- ch-covidcertificate-backend-verifier/ch-covidcertificate-backend-verifier-model/src/main/java/ch/admin/bag/covidcertificate/backend/verifier/model/cert/ActiveCertsResponse.java
+++ ch-covidcertificate-backend-verifier/ch-covidcertificate-backend-verifier-model/src/main/java/ch/admin/bag/covidcertificate/backend/verifier/model/cert/ActiveCertsResponse.java
@@ -1,10 +1,18 @@
 package ch.admin.bag.covidcertificate.backend.verifier.model.cert;
 
+import ch.ubique.openapi.docannotations.Documentation;
+import java.time.Duration;
 import java.util.ArrayList;
 import java.util.List;
 
 public class ActiveCertsResponse {
+    @Documentation(description = "list of active key ids")
     private List<String> activeKeyIds = new ArrayList<>();
+
+    @Documentation(
+            description = "describes how long the list response is valid for in ms",
+            example = "172800000")
+    private Duration validDuration = Duration.ofHours(48);
 
     public ActiveCertsResponse(List<String> activeKeyIds) {
         if (activeKeyIds == null) {
@@ -19,5 +27,13 @@ public class ActiveCertsResponse {
 
     public void setActiveKeyIds(List<String> activeKeyIds) {
         this.activeKeyIds = activeKeyIds;
+    }
+
+    public Long getValidDuration() {
+        return validDuration.toMillis();
+    }
+
+    public void setValidDuration(Long durationInMs) {
+        this.validDuration = Duration.ofMillis(durationInMs);
     }
 }
