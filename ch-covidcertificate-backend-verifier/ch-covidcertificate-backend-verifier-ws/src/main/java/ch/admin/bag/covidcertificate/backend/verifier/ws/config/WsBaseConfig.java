61ac8fe032e8f489d2bb040801d6656d2d353811
Felix Haller <haller@ubique.ch>
2021-06-10 11:04:38 +0200

    add jws message converter

--- ch-covidcertificate-backend-verifier/ch-covidcertificate-backend-verifier-ws/src/main/java/ch/admin/bag/covidcertificate/backend/verifier/ws/config/WsBaseConfig.java
+++ ch-covidcertificate-backend-verifier/ch-covidcertificate-backend-verifier-ws/src/main/java/ch/admin/bag/covidcertificate/backend/verifier/ws/config/WsBaseConfig.java
@@ -16,10 +16,19 @@ import ch.admin.bag.covidcertificate.backend.verifier.ws.controller.KeyControlle
 import ch.admin.bag.covidcertificate.backend.verifier.ws.controller.RevocationListController;
 import ch.admin.bag.covidcertificate.backend.verifier.ws.controller.VerificationRulesController;
 import ch.admin.bag.covidcertificate.backend.verifier.ws.interceptor.HeaderInjector;
+import ch.admin.bag.covidcertificate.backend.verifier.ws.security.signature.JwsMessageConverter;
 import ch.admin.bag.covidcertificate.backend.verifier.ws.utils.CacheUtil;
 import ch.admin.bag.covidcertificate.backend.verifier.ws.utils.RestTemplateHelper;
+import java.io.ByteArrayInputStream;
 import java.io.IOException;
+import java.security.KeyStore;
+import java.security.KeyStoreException;
+import java.security.NoSuchAlgorithmException;
+import java.security.UnrecoverableKeyException;
+import java.security.cert.CertificateException;
 import java.time.Duration;
+import java.util.Base64;
+import java.util.List;
 import java.util.Map;
 import javax.sql.DataSource;
 import org.flywaydb.core.Flyway;
@@ -28,6 +37,7 @@ import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.http.converter.HttpMessageConverter;
 import org.springframework.web.client.RestTemplate;
 import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
@@ -44,6 +54,12 @@ public abstract class WsBaseConfig implements WebMvcConfigurer {
     @Value("${revocationList.baseurl}")
     String revokedCertsBaseUrl;
 
+    @Value("${ws.jws.p12:}")
+    public String p12KeyStore;
+
+    @Value("${ws.jws.password:}")
+    public String p12KeyStorePassword;
+
     public abstract DataSource dataSource();
 
     public abstract Flyway flyway();
@@ -58,6 +74,29 @@ public abstract class WsBaseConfig implements WebMvcConfigurer {
     @Value("${ws.verificationRules.max-age:PT1M}")
     public void setVerificationRulesMaxAge(Duration maxAge) {
         CacheUtil.VERIFICATION_RULES_MAX_AGE = maxAge;
+    }
+
+    @Override
+    public void extendMessageConverters(List<HttpMessageConverter<?>> converters) {
+        try {
+            converters.add(
+                    new JwsMessageConverter(jwsKeyStore(), p12KeyStorePassword.toCharArray()));
+        } catch (KeyStoreException
+                | NoSuchAlgorithmException
+                | CertificateException
+                | IOException
+                | UnrecoverableKeyException e) {
+            logger.error("Could not load key store", e);
+            throw new RuntimeException("Could not add jws Converter");
+        }
+    }
+
+    public KeyStore jwsKeyStore()
+            throws KeyStoreException, NoSuchAlgorithmException, CertificateException, IOException {
+        var keyStore = KeyStore.getInstance("pkcs12");
+        var bais = new ByteArrayInputStream(Base64.getDecoder().decode(p12KeyStore));
+        keyStore.load(bais, p12KeyStorePassword.toCharArray());
+        return keyStore;
     }
 
     @Bean
