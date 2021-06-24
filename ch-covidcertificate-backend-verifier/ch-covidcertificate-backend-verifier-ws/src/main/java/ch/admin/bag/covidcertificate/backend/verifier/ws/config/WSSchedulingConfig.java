7888c9d8e3328536d5fcfa2ff207a93a1b1a2889
lmeinen <lmeinen@student.ethz.ch>
2021-06-24 14:08:54 +0200

    Added scheduled call to app token dataservice

---
+++ ch-covidcertificate-backend-verifier/ch-covidcertificate-backend-verifier-ws/src/main/java/ch/admin/bag/covidcertificate/backend/verifier/ws/config/WSSchedulingConfig.java
@@ -0,0 +1,29 @@
+package ch.admin.bag.covidcertificate.backend.verifier.ws.config;
+
+import ch.admin.bag.covidcertificate.backend.verifier.data.AppTokenDataService;
+import ch.admin.bag.covidcertificate.backend.verifier.model.AppToken;
+import ch.admin.bag.covidcertificate.backend.verifier.ws.config.model.ApiKeyConfig;
+import java.util.List;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.scheduling.annotation.EnableScheduling;
+import org.springframework.scheduling.annotation.Scheduled;
+
+@Configuration
+@EnableScheduling
+public class WSSchedulingConfig {
+
+    private final ApiKeyConfig apiKeyConfig;
+    private final AppTokenDataService appTokenDataService;
+
+    public WSSchedulingConfig(
+        ApiKeyConfig apiKeyConfig,
+        AppTokenDataService appTokenDataService) {
+        this.apiKeyConfig = apiKeyConfig;
+        this.appTokenDataService = appTokenDataService;
+    }
+
+    @Scheduled(cron = "${ws.authentication.cron:0 0/5 0 ? * *}")
+    public void updateAppTokens() {
+        final var appTokens = appTokenDataService.getAppTokens();
+    }
+}
