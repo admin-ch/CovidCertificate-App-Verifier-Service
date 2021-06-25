d4a566a8121fb9ad280f10967353f1249174e782
lmeinen <lmeinen@student.ethz.ch>
2021-06-25 11:57:42 +0200

    Use tokens stored in database as ApiKeyConfig

--- ch-covidcertificate-backend-verifier/ch-covidcertificate-backend-verifier-ws/src/main/java/ch/admin/bag/covidcertificate/backend/verifier/ws/config/WSSchedulingConfig.java
+++ ch-covidcertificate-backend-verifier/ch-covidcertificate-backend-verifier-ws/src/main/java/ch/admin/bag/covidcertificate/backend/verifier/ws/config/WSSchedulingConfig.java
@@ -3,7 +3,9 @@ package ch.admin.bag.covidcertificate.backend.verifier.ws.config;
 import ch.admin.bag.covidcertificate.backend.verifier.data.AppTokenDataService;
 import ch.admin.bag.covidcertificate.backend.verifier.model.AppToken;
 import ch.admin.bag.covidcertificate.backend.verifier.ws.config.model.ApiKeyConfig;
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.scheduling.annotation.EnableScheduling;
 import org.springframework.scheduling.annotation.Scheduled;
@@ -25,5 +27,10 @@ public class WSSchedulingConfig {
     @Scheduled(cron = "${ws.authentication.cron:0 0/5 0 ? * *}")
     public void updateAppTokens() {
         final var appTokens = appTokenDataService.getAppTokens();
+        final Map<String, String> apiKeys = new HashMap<>();
+        for (var token: appTokens) {
+            apiKeys.put(token.getDescription(), token.getApiKey());
+        }
+        apiKeyConfig.setApiKeys(apiKeys);
     }
 }
