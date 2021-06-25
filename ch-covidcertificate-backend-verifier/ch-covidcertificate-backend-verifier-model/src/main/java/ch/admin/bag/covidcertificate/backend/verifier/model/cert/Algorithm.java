c024a318523a67501a3be54fde5425b561587f0b
lmeinen <lmeinen@student.ethz.ch>
2021-06-25 14:23:28 +0200

    Removed unnecessary Algorithm enum members

--- ch-covidcertificate-backend-verifier/ch-covidcertificate-backend-verifier-model/src/main/java/ch/admin/bag/covidcertificate/backend/verifier/model/cert/Algorithm.java
+++ ch-covidcertificate-backend-verifier/ch-covidcertificate-backend-verifier-model/src/main/java/ch/admin/bag/covidcertificate/backend/verifier/model/cert/Algorithm.java
@@ -3,21 +3,7 @@ package ch.admin.bag.covidcertificate.backend.verifier.model.cert;
 public enum Algorithm {
     ES256,
     RS256,
-    PS256,
     UNSUPPORTED;
-
-    public static Algorithm forSigAlgName(String sigAlgName) {
-        if (sigAlgName.contains("RSA")) {
-            if (sigAlgName.contains("SHA256")) {
-                return RS256;
-            } else if (sigAlgName.contains("PSS")) {
-                return PS256;
-            }
-        } else if (sigAlgName.contains("ECDSA") && sigAlgName.contains("SHA256")) {
-            return ES256;
-        }
-        return UNSUPPORTED;
-    }
 
     public static Algorithm forPubKeyType(String keyType) {
         if ("RSA".equals(keyType)) {
