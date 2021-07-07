c067a5f1bcbc22a4660d9204eb4a5bf83e833198
Fabian Aggeler <aggeler@ubique.ch>
2021-07-07 22:44:44 +0200

    Load app-tokens from DB only if "app-token-db" profile is active

--- ch-covidcertificate-backend-verifier/ch-covidcertificate-backend-verifier-ws/src/main/java/ch/admin/bag/covidcertificate/backend/verifier/ws/config/WSSchedulingConfig.java
+++ ch-covidcertificate-backend-verifier/ch-covidcertificate-backend-verifier-ws/src/main/java/ch/admin/bag/covidcertificate/backend/verifier/ws/config/WSSchedulingConfig.java
@@ -7,9 +7,11 @@ import java.util.Map;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Profile;
 import org.springframework.scheduling.annotation.EnableScheduling;
 import org.springframework.scheduling.annotation.Scheduled;
 
+@Profile("app-token-db")
 @Configuration
 @EnableScheduling
 public class WSSchedulingConfig {
