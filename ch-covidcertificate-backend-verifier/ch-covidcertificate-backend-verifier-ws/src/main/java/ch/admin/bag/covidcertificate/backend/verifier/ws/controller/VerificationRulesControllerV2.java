cadc9a5d089438d06455a7e3c952a0af388f6c54
Patrick Amrein <amrein@ubique.ch>
2021-08-25 08:37:07 +0200

    add controller for new format

---
+++ ch-covidcertificate-backend-verifier/ch-covidcertificate-backend-verifier-ws/src/main/java/ch/admin/bag/covidcertificate/backend/verifier/ws/controller/VerificationRulesControllerV2.java
@@ -0,0 +1,62 @@
+/*
+ * Copyright (c) 2021 Ubique Innovation AG <https://www.ubique.ch>
+ *
+ * This Source Code Form is subject to the terms of the Mozilla Public
+ * License, v. 2.0. If a copy of the MPL was not distributed with this
+ * file, You can obtain one at https://mozilla.org/MPL/2.0/.
+ *
+ * SPDX-License-Identifier: MPL-2.0
+ */
+
+package ch.admin.bag.covidcertificate.backend.verifier.ws.controller;
+
+import java.io.File;
+import java.io.IOException;
+import java.security.NoSuchAlgorithmException;
+import java.util.Map;
+
+import com.fasterxml.jackson.databind.ObjectMapper;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.core.io.ClassPathResource;
+import org.springframework.http.CacheControl;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.context.request.WebRequest;
+
+import ch.admin.bag.covidcertificate.backend.verifier.ws.utils.CacheUtil;
+import ch.admin.bag.covidcertificate.backend.verifier.ws.utils.EtagUtil;
+import ch.ubique.openapi.docannotations.Documentation;
+
+@Controller
+@RequestMapping("trust/v2")
+public class VerificationRulesControllerV2 {
+    private static final Logger logger = LoggerFactory.getLogger(VerificationRulesController.class);
+
+    private final Map verificationRules;
+    private final String verificationRulesEtag;
+
+    public VerificationRulesControllerV2() throws IOException, NoSuchAlgorithmException {
+        ObjectMapper mapper = new ObjectMapper();
+        File verificationRulesFile = new ClassPathResource("verificationRulesV2.json").getFile();
+        this.verificationRules = mapper.readValue(verificationRulesFile, Map.class);
+        this.verificationRulesEtag = EtagUtil.getSha1HashForFiles(verificationRulesFile.getPath());
+    }
+
+    @Documentation(description = "get list of verification rules (uses the new format)", responses = { "200 => list of verification rules",
+            "304 => no changes since last request" }, responseHeaders = { "ETag:etag to set for next request:string" })
+    @GetMapping(value = "/verificationRules")
+    public @ResponseBody ResponseEntity<Map> getVerificationRules(WebRequest request) {
+        if (request.checkNotModified(verificationRulesEtag)) {
+            return ResponseEntity.status(HttpStatus.NOT_MODIFIED).build();
+        }
+        return ResponseEntity.ok().cacheControl(CacheControl.maxAge(CacheUtil.VERIFICATION_RULES_MAX_AGE))
+                .body(verificationRules);
+    }
+}
+
