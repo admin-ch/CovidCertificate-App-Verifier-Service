6e5f79296f43c399bc6feb951c3275282fb4138e
Daniel Gstöhl <gstoehl@ubique.ch>
2021-11-26 18:12:36 +0100

    update validfrom dates in new rule format

--- ch-covidcertificate-backend-verifier/ch-covidcertificate-backend-verifier-valuesets/src/test/java/ch/admin/bag/covidcertificate/backend/verifier/valuesets/VerificationRulesTest.java
+++ ch-covidcertificate-backend-verifier/ch-covidcertificate-backend-verifier-valuesets/src/test/java/ch/admin/bag/covidcertificate/backend/verifier/valuesets/VerificationRulesTest.java
@@ -17,25 +17,26 @@ import com.fasterxml.jackson.core.util.DefaultPrettyPrinter;
 import com.fasterxml.jackson.databind.JsonNode;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import com.fasterxml.jackson.databind.node.ArrayNode;
-import com.fasterxml.jackson.databind.node.JsonNodeFactory;
 import com.fasterxml.jackson.databind.node.ObjectNode;
 import com.fasterxml.jackson.datatype.jsr310.JavaTimeModule;
 import java.io.File;
-import java.io.FilenameFilter;
 import java.io.IOException;
+import java.io.InputStream;
+import java.nio.file.Paths;
 import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.HashMap;
+import java.util.HashSet;
 import java.util.Iterator;
 import java.util.LinkedHashMap;
 import java.util.List;
 import java.util.Map;
 import java.util.Map.Entry;
-import java.util.stream.Collectors;
+import java.util.Set;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
 import org.apache.commons.lang3.StringUtils;
-import org.junit.internal.Classes;
 import org.junit.jupiter.api.BeforeAll;
-import org.junit.jupiter.api.Disabled;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.TestInstance;
 import org.junit.jupiter.api.TestInstance.Lifecycle;
@@ -49,14 +50,27 @@ public class VerificationRulesTest {
     private static final Logger logger = LoggerFactory.getLogger(VerificationRulesTest.class);
     private ObjectMapper mapper;
 
+    private static final String RULE_VERSION = "1.0.5";
+    private static final String RULE_VALID_FROM = "2021-12-03T12:00:00Z";
+
     private static final String RULES_V1_PATH = "src/main/resources/verificationRules.json";
     private static final String RULES_V2_PATH = "src/main/resources/verificationRulesV2.json";
     private static final String RULES_UPLOAD_PATH =
             "src/main/resources/verificationRulesUpload.json";
-    private static final String RULES_MASTER_CLASSPATH = "verificationRulesMaster.json";
-    private static final String RULES_MASTER_DIR = "master-rules";
-    private static final String CH_ONLY_DIR = "ch-only-rules";
+    private static final String MASTER_TEMPLATE_CLASSPATH = "templates/masterTemplate.json";
+    private static final String RULE_TEMPLATE_CLASSPATH = "templates/ruleTemplate.json";
+    private static final String VERIFICATION_RULES_SOURCE_DIR = "verification-rules";
+    private static final String VERIFICATION_RULES_COMPILE_DIR = "generated/verification-rules";
+    private static final String DISPLAY_RULES_COMPILE_DIR = "generated/display-rules";
+    private static final String CH_ONLY_RULES_COMPILE_DIR = "generated/ch-only-rules";
     private static final String EU_TEST_RULES_OUTPUT_PATH = "src/main/resources/CH/";
+
+    // matches multiline comments without the leading and trailing /* and */
+    private static final Pattern commentPattern =
+            Pattern.compile("(?<!\\/)(?<=\\/\\*)((?:(?!\\*\\/).|\\s)*)(?=\\*\\/)");
+
+    // matches the part after the payload. in "payload.v.0.foo"
+    private static final Pattern payloadPattern = Pattern.compile("(?<=\"payload\\.).*?(?=\")");
 
     @BeforeAll
     public void setup() throws Exception {
@@ -71,103 +85,124 @@ public class VerificationRulesTest {
     @Test
     @EnabledIfEnvironmentVariable(named = "COVIDCERT_GENERATE_VALIDATION_RULES", matches = "1")
     public void generateRulesJsons() throws Exception {
-        JsonNode v2 = mapMasterToV2();
+        JsonNode v2 = generateV2();
         mapV2RulesToUpload(v2);
         mapV2RulesToV1(v2);
     }
 
-    private JsonNode mapMasterToV2() throws Exception {
-        JsonNode master =
-                mapper.readTree(new ClassPathResource(RULES_MASTER_CLASSPATH).getInputStream());
+    private JsonNode generateV2() throws Exception {
+        JsonNode v2 =
+                mapper.readTree(new ClassPathResource(MASTER_TEMPLATE_CLASSPATH).getInputStream());
 
-        List<JsonNode> rules =
-                Arrays.stream(new ClassPathResource(RULES_MASTER_DIR).getFile().list())
-                        .filter(filename -> filename.endsWith(".json"))
-                        .sorted()
-                        .map(
-                                filename -> {
-                                    try {
-                                        return mapper.readTree(
-                                                new ClassPathResource(
-                                                                RULES_MASTER_DIR + "/" + filename)
-                                                        .getInputStream());
-                                    } catch (IOException e) {
-                                        e.printStackTrace();
-                                        return null;
-                                    }
-                                })
-                        .collect(Collectors.toList());
-        ((ObjectNode) master).putArray("rules").addAll(rules);
-        // Insert rules for display logic, for tests and vaccines only valid in
-        // switzerland
-        // NOTE: we need to use DeMorgans rule to desugar 'or' since CertLogic does not support it
-        List<JsonNode> chDisplayRules =
-                Arrays.stream(new ClassPathResource(CH_ONLY_DIR).getFile().list())
-                        .filter(filename -> filename.endsWith(".json"))
-                        .sorted()
-                        .map(
-                                filename -> {
-                                    try {
-                                        ObjectNode node = new ObjectNode(JsonNodeFactory.instance);
-                                        node.putArray("!").add(
+        String[] logicFiles =
+                (new ClassPathResource(VERIFICATION_RULES_COMPILE_DIR).getFile().list());
+        List<ObjectNode> rules = new ArrayList<>();
+        for (String filename : logicFiles) {
+            ObjectNode rule =
+                    (ObjectNode)
+                            mapper.readTree(
+                                    new ClassPathResource(RULE_TEMPLATE_CLASSPATH)
+                                            .getInputStream());
+            switch (filename.charAt(0)) {
+                case 'G':
+                    rule.put("certificateType", "General");
+                    break;
+                case 'T':
+                    rule.put("certificateType", "Test");
+                    break;
+                case 'R':
+                    rule.put("certificateType", "Recovery");
+                    break;
+                case 'V':
+                    rule.put("certificateType", "Vaccination");
+                    break;
+                default:
+                    logger.error("Rule file name does not start with G, R, T or V: ?{}", filename);
+            }
+            String ruleId = filename.split("\\.")[0];
+            InputStream sourceFile =
+                    new ClassPathResource(
+                                    Paths.get(VERIFICATION_RULES_SOURCE_DIR, ruleId.concat(".aifc"))
+                                            .toString())
+                            .getInputStream();
+            String sourceFileContent = new String(sourceFile.readAllBytes());
+            Matcher matcher = commentPattern.matcher(sourceFileContent);
+            if (matcher.find()) {
+                String description = matcher.group();
+                ((ObjectNode) (((ArrayNode) rule.get("description")).get(0)))
+                        .put("desc", description);
+            } else {
+                logger.warn(
+                        "File contained no comment, rule description will be empty {}", filename);
+            }
+            // File compiledFile = new ClassPathResource(.toString()).getFile();
+            ObjectNode logic =
+                    (ObjectNode)
+                            mapper.readTree(
+                                    new ClassPathResource(
+                                                    Paths.get(
+                                                                    VERIFICATION_RULES_COMPILE_DIR,
+                                                                    filename)
+                                                            .toString())
+                                            .getInputStream());
+            rule.set("logic", logic);
+            Matcher payloadMatcher = payloadPattern.matcher(logic.toString());
+            Set<String> affectedFields = new HashSet<>();
+            while (payloadMatcher.find()) {
+                affectedFields.add(payloadMatcher.group());
+            }
+            affectedFields.forEach(field -> ((ArrayNode) rule.get("affectedFields")).add(field));
+            rule.put("identifier", ruleId);
+            rule.put("validFrom", RULE_VALID_FROM);
+            rule.put("version", RULE_VERSION);
+            rules.add(rule);
+        }
+
+        String[] displayRuleFiles =
+                (new ClassPathResource(DISPLAY_RULES_COMPILE_DIR).getFile().list());
+        List<ObjectNode> displayRules = new ArrayList<>();
+        for (String filename : displayRuleFiles) {
+            ObjectNode rule = ((ArrayNode) v2.get("displayRules")).addObject();
+            rule.put("id", filename.split("\\.")[0]);
+            rule.set(
+                    "logic",
+                    mapper.readTree(
+                            new ClassPathResource(
+                                            Paths.get(DISPLAY_RULES_COMPILE_DIR, filename)
+                                                    .toString())
+                                    .getInputStream()));
+            displayRules.add(rule);
+        }
+
+        ObjectNode chOnlyDisplayRule = ((ArrayNode) v2.get("displayRules")).addObject();
+        chOnlyDisplayRule.put("id", "is-only-valid-in-ch");
+        ObjectNode logic = chOnlyDisplayRule.putObject("logic");
+        ArrayNode notAndArray = logic.putArray("!").addObject().putArray("and");
+        Arrays.stream(new ClassPathResource(CH_ONLY_RULES_COMPILE_DIR).getFile().list())
+                .filter(filename -> filename.endsWith(".json"))
+                .sorted()
+                .forEach(
+                        filename -> {
+                            try {
+                                ObjectNode node = notAndArray.addObject();
+                                node.putArray("!")
+                                        .add(
                                                 mapper.readTree(
-                                                new ClassPathResource(CH_ONLY_DIR + "/" + filename)
-                                                        .getInputStream()));
-                                        return node;
-                                    } catch (IOException e) {
-                                        e.printStackTrace();
-                                        return null;
-                                    }
-                                })
-                        .collect(Collectors.toList());
-        master.get("displayRules")
-                .elements()
-                .forEachRemaining(
-                        (displayRule -> {
-                            if (displayRule.get("id").asText().equals("is-only-valid-in-ch")) {
-                                ((ObjectNode) displayRule.get("logic").get("!").get(0))
-                                        .putArray("and")
-                                        .addAll(chDisplayRules);
+                                                        new ClassPathResource(
+                                                                        CH_ONLY_RULES_COMPILE_DIR
+                                                                                + "/"
+                                                                                + filename)
+                                                                .getInputStream()));
+                            } catch (IOException e) {
+                                logger.error(
+                                        "Something failed while generating only-valid-in-ch rules",
+                                        e);
                             }
-                        }));
-
-        ObjectNode v2 = master.deepCopy();
-        resolvedInlineVars(v2, master);
+                        });
+        ArrayNode rulesArray = (ArrayNode) v2.get("rules");
+        rulesArray.addAll(rules);
         mapper.writerWithDefaultPrettyPrinter().writeValue(new File(RULES_V2_PATH), v2);
         return v2;
-    }
-
-    private void resolvedInlineVars(JsonNode resolved, JsonNode master) {
-        if (resolved.isArray()) {
-            for (JsonNode element : resolved) {
-                resolvedInlineVars(element, master);
-            }
-        } else if (resolved.isObject()) {
-            Map<String, JsonNode> resolvedObjects = new LinkedHashMap<>();
-            Iterator<Entry<String, JsonNode>> fieldIterator = resolved.fields();
-            while (fieldIterator.hasNext()) {
-                Entry<String, JsonNode> entry = fieldIterator.next();
-                String key = entry.getKey();
-                JsonNode value = entry.getValue();
-                if (key.equals("var")) {
-                    if (value.isTextual()) {
-                        if (value.textValue().startsWith("external.valueSets")) {
-                            resolvedObjects.put(
-                                    key,
-                                    master.at(
-                                            value.textValue()
-                                                    .substring("external".length())
-                                                    .replace(".", "/")));
-                        }
-                    }
-                } else {
-                    resolvedInlineVars(value, master);
-                }
-            }
-            for (Entry<String, JsonNode> resolvedEntry : resolvedObjects.entrySet()) {
-                ((ObjectNode) resolved).set(resolvedEntry.getKey(), resolvedEntry.getValue());
-            }
-        }
     }
 
     private void mapV2RulesToUpload(JsonNode v2) throws Exception {
