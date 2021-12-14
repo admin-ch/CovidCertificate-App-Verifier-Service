ce6222a68660e0d6aa1780c2e3e5f6437e1504a3
Martin Alig <alig@ubique.ch>
2021-12-14 17:28:01 +0100

    Update Spring Boot Version
    Update Versions. Fix Annotation

--- ch-covidcertificate-backend-verifier/ch-covidcertificate-backend-verifier-ws/src/main/java/ch/admin/bag/covidcertificate/backend/verifier/ws/config/ActuatorSecurity.java
+++ ch-covidcertificate-backend-verifier/ch-covidcertificate-backend-verifier-ws/src/main/java/ch/admin/bag/covidcertificate/backend/verifier/ws/config/ActuatorSecurity.java
@@ -11,7 +11,6 @@
 package ch.admin.bag.covidcertificate.backend.verifier.ws.config;
 
 import ch.admin.bag.covidcertificate.backend.verifier.ws.config.configbeans.ActuatorSecurityConfig;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.actuate.health.HealthEndpoint;
 import org.springframework.boot.actuate.info.InfoEndpoint;
@@ -33,56 +32,54 @@ import org.springframework.security.config.annotation.web.configuration.WebSecur
 @EnableWebSecurity
 public class ActuatorSecurity extends WebSecurityConfigurerAdapter {
 
-    private static final String PROMETHEUS_ROLE = "PROMETHEUS";
+  private static final String PROMETHEUS_ROLE = "PROMETHEUS";
 
-    @Value("${ws.monitor.prometheus.user}")
-    private String user;
+  @Value("${ws.monitor.prometheus.user}")
+  private String user;
 
-    @Value("${ws.monitor.prometheus.password}")
-    private String password;
+  @Value("${ws.monitor.prometheus.password}")
+  private String password;
 
-    @Bean
-    ActuatorSecurityConfig passwordDefault() {
-        return new ActuatorSecurityConfig(user, password);
-    }
+  @Bean
+  ActuatorSecurityConfig passwordDefault() {
+    return new ActuatorSecurityConfig(user, password);
+  }
 
-    @Override
-    protected void configure(HttpSecurity http) throws Exception {
-        http.requestMatcher(
-                        org.springframework.boot.actuate.autoconfigure.security.servlet
-                                .EndpointRequest.toAnyEndpoint())
-                .authorizeRequests()
-                .requestMatchers(
-                        org.springframework.boot.actuate.autoconfigure.security.servlet
-                                .EndpointRequest.to(HealthEndpoint.class))
-                .permitAll()
-                .requestMatchers(
-                        org.springframework.boot.actuate.autoconfigure.security.servlet
-                                .EndpointRequest.to(InfoEndpoint.class))
-                .permitAll()
-                .requestMatchers(
-                        org.springframework.boot.actuate.autoconfigure.security.servlet
-                                .EndpointRequest.to(LoggersEndpoint.class))
-                .hasRole(PROMETHEUS_ROLE)
-                .requestMatchers(
-                        org.springframework.boot.actuate.autoconfigure.security.servlet
-                                .EndpointRequest.to(PrometheusScrapeEndpoint.class))
-                .hasRole(PROMETHEUS_ROLE)
-                .anyRequest()
-                .denyAll()
-                .and()
-                .httpBasic();
+  @Override
+  protected void configure(HttpSecurity http) throws Exception {
+    http.requestMatcher(
+            org.springframework.boot.actuate.autoconfigure.security.servlet.EndpointRequest
+                .toAnyEndpoint())
+        .authorizeRequests()
+        .requestMatchers(
+            org.springframework.boot.actuate.autoconfigure.security.servlet.EndpointRequest.to(
+                HealthEndpoint.class))
+        .permitAll()
+        .requestMatchers(
+            org.springframework.boot.actuate.autoconfigure.security.servlet.EndpointRequest.to(
+                InfoEndpoint.class))
+        .permitAll()
+        .requestMatchers(
+            org.springframework.boot.actuate.autoconfigure.security.servlet.EndpointRequest.to(
+                LoggersEndpoint.class))
+        .hasRole(PROMETHEUS_ROLE)
+        .requestMatchers(
+            org.springframework.boot.actuate.autoconfigure.security.servlet.EndpointRequest.to(
+                PrometheusScrapeEndpoint.class))
+        .hasRole(PROMETHEUS_ROLE)
+        .anyRequest()
+        .denyAll()
+        .and()
+        .httpBasic();
 
-        http.csrf().ignoringAntMatchers("/actuator/loggers/**");
-    }
+    http.csrf().ignoringAntMatchers("/actuator/loggers/**");
+  }
 
-    @Autowired
-    protected void configureGlobal(
-            AuthenticationManagerBuilder auth, ActuatorSecurityConfig securityConfig)
-            throws Exception {
-        auth.inMemoryAuthentication()
-                .withUser(securityConfig.getUsername())
-                .password(securityConfig.getPassword())
-                .roles(PROMETHEUS_ROLE);
-    }
+  protected void configureGlobal(
+      AuthenticationManagerBuilder auth, ActuatorSecurityConfig securityConfig) throws Exception {
+    auth.inMemoryAuthentication()
+        .withUser(securityConfig.getUsername())
+        .password(securityConfig.getPassword())
+        .roles(PROMETHEUS_ROLE);
+  }
 }
