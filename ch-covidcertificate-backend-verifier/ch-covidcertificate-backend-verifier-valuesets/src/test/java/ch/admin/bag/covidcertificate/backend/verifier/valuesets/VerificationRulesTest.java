bc300b2c0df92f1ba49f37b0dc15024b01fa2ae2
Daniel Gstöhl <gstoehl@ubique.ch>
2022-03-02 09:20:12 +0100

    bump rules version

--- ch-covidcertificate-backend-verifier/ch-covidcertificate-backend-verifier-valuesets/src/test/java/ch/admin/bag/covidcertificate/backend/verifier/valuesets/VerificationRulesTest.java
+++ ch-covidcertificate-backend-verifier/ch-covidcertificate-backend-verifier-valuesets/src/test/java/ch/admin/bag/covidcertificate/backend/verifier/valuesets/VerificationRulesTest.java
@@ -84,8 +84,8 @@ public class VerificationRulesTest {
     private static final Logger logger = LoggerFactory.getLogger(VerificationRulesTest.class);
     private ObjectMapper mapper;
 
-    private static final String RULE_VERSION = "1.0.11";
-    private static final String RULE_VALID_FROM = "2022-03-04T00:00:00Z";
+    private static final String RULE_VERSION = "1.0.12";
+    private static final String RULE_VALID_FROM = "2022-03-06T00:00:00Z";
 
     private static final String RULES_V1_PATH = "src/main/resources/verificationRules.json";
     private static final String RULES_V2_PATH = "src/main/resources/verificationRulesV2.json";
